e9300adb577a5153812ca8e8e75e5a7260b07f2d
pragyan154 <59705399+pragyan154@users.noreply.github.com>
2020-02-13 12:14:05 -0800

    hackerrank Bon Appetit python code

---
+++ Bon_Appetit.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+    actual = (sum(bill) - bill[k])/2 
+    (overcharged) = (b) - (actual)
+    if overcharged == 0:
+        print("Bon Appetit")
+    else:
+        print(int(overcharged))
+
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
