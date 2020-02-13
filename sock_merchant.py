3b0663a27b11d0d6624864be9d895ac0150e2182
pragyan154 <59705399+pragyan154@users.noreply.github.com>
2020-02-13 14:22:39 -0800

    Update sock_merchant.py

--- sock_merchant.py
+++ sock_merchant.py
@@ -1,5 +1,3 @@
- 
-
 #!/bin/python3
 
 import math
@@ -9,23 +7,19 @@ import re
 import sys
 
 # Complete the sockMerchant function below.
-def sockMerchant(n , ar ):
+def sockMerchant(n, ar):
     numbers = list(set(ar))
     total_pair = 0
-    for i  in numbers:
-        total_pair += ar.count(i)//2
-    
+    for i in numbers:
+        if ar.count(i)> 1 :
+            pairs = ar.count(i)//2
+            total_pair += pairs
+            
 
     return total_pair
-
-    
-
+            
 
 
-        
-
-
-    return(total)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
