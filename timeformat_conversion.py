e903772aad60cb88dc86c435e5a58084d9c74811
pragyan154 <59705399+pragyan154@users.noreply.github.com>
2020-02-16 08:34:20 -0800

    Update timeformat_conversion.py

--- timeformat_conversion.py
+++ timeformat_conversion.py
@@ -1,1 +1,39 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the timeConversion function below.
+#
+def timeConversion(s):
+    #
+    # Write your code here.
+    time = int(12)
+
+    
+    if s[-2:] == "AM" and s[:2] == "12":
+
+        return ("00" + s[2:-2])
+        
+    elif s[-2:] == "AM":
+        return (s[:-2])
+
+    elif s[-2:] == "PM" and s[:2] == "12":
+        return (s[:-2])
+
+    else:
+        return str(time+int(s[:2])) + s[2:8]
+        
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    f.write(result + '\n')
+
+    f.close()
 
